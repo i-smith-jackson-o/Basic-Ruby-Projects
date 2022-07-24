bbf6be8220e561bd3a7704f2f14f29a88ae4543a
i-smith-jackson-o <i.smith.jackson.o@gmail.com>
2022-07-24 01:15:31 -0400

    init

---
+++ caesar_cipher.rb
@@ -0,0 +1,18 @@
+def caesar_cipher(string, shift=0)
+  capital = ("A".."Z").to_a #65-90
+  lower = ("a".."z").to_a #97-122
+  split_array = string.split("")
+
+  shift_char = split_array.map do |char|
+    if capital.include?(char)
+      char = capital[(capital.find_index(char) + shift) % 26] 
+    elsif lower.include?(char)
+      char = lower[(lower.find_index(char) + shift)  % 26] 
+    else     
+      char
+    end 
+  end 
+  shift_char.join("")
+end 
+
+puts caesar_cipher("Programming Ruby", 5)
